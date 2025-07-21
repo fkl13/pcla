ad754f70cb3d83f0227dfc4ad4b2233509e4dda1
Fabian Klaiber <fklaiber@posteo.net>
2025-07-21 15:11:49 +0200

    Add file flag to wc tool

--- wc/main.go
+++ wc/main.go
@@ -11,9 +11,23 @@ import (
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
 	bytes := flag.Bool("b", false, "Count bytes")
+	filename := flag.String("file", "", "Read from file")
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines, *bytes))
+	reader := os.Stdin
+
+	if *filename != "" {
+		file, err := os.Open(*filename)
+		if err != nil {
+			fmt.Println("Couldn't read file")
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		reader = file
+	}
+
+	fmt.Println(count(reader, *lines, *bytes))
 }
 
 func count(r io.Reader, countLines, countBytes bool) int {
