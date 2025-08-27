90933690f9201621587090419d75472b1258cf70
Fabian Klaiber <fklaiber@posteo.net>
2025-08-27 11:19:54 +0200

    Add integration test

--- cobra/pScan/cmd/actions_test.go
+++ cobra/pScan/cmd/actions_test.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/fkl13/pcla/cobra/pScan/scan"
@@ -82,5 +84,58 @@ func TestHostActions(t *testing.T) {
 				t.Errorf("Expected output %q, got %q\n", tc.expectedOut, out.String())
 			}
 		})
+	}
+}
+
+func TestIntegration(t *testing.T) {
+	hosts := []string{
+		"host1",
+		"host2",
+		"host3",
+	}
+
+	tf, cleanup := setup(t, hosts, false)
+	defer cleanup()
+
+	delHost := "host2"
+	hostsEnd := []string{
+		"host1",
+		"host3",
+	}
+
+	var out bytes.Buffer
+
+	expectedOut := ""
+	for _, v := range hosts {
+		expectedOut += fmt.Sprintf("Added host: %s\n", v)
+	}
+	expectedOut += strings.Join(hosts, "\n")
+	expectedOut += fmt.Sprintln()
+	expectedOut += fmt.Sprintf("Deleted host: %s\n", delHost)
+	expectedOut += strings.Join(hostsEnd, "\n")
+	expectedOut += fmt.Sprintln()
+
+	// Add hosts to the list
+	if err := addAction(&out, tf, hosts); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	// List hosts
+	if err := listAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	// Delete host2
+	if err := deleteAction(&out, tf, []string{delHost}); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	// List hosts after delete
+	if err := listAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	if out.String() != expectedOut {
+		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
 	}
 }
