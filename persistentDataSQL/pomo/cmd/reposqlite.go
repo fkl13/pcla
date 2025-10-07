5b9267272b4bf31f42431fe7b6a88e7e0b6b3783
Fabian Klaiber <fklaiber@posteo.net>
2025-10-07 15:56:58 +0200

    Add command line flags for sqlite db

---
+++ persistentDataSQL/pomo/cmd/reposqlite.go
@@ -0,0 +1,18 @@
+//go:build !inmemory
+// +build !inmemory
+
+package cmd
+
+import (
+	"github.com/fkl13/pcla/persistentDataSQL/pomo/pomodoro"
+	"github.com/fkl13/pcla/persistentDataSQL/pomo/pomodoro/repository"
+	"github.com/spf13/viper"
+)
+
+func getRepo() (pomodoro.Repository, error) {
+	repo, err := repository.NewSQLite3Repo(viper.GetString("db"))
+	if err != nil {
+		return nil, err
+	}
+	return repo, nil
+}
