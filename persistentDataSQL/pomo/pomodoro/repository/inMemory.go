350850da86276b6eaa77e3507fe2859c17c38e37
Fabian Klaiber <fklaiber@posteo.net>
2025-10-08 22:23:06 +0200

    Compute summary

--- persistentDataSQL/pomo/pomodoro/repository/inMemory.go
+++ persistentDataSQL/pomo/pomodoro/repository/inMemory.go
@@ -5,7 +5,9 @@ package repository
 
 import (
 	"fmt"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/fkl13/pcla/persistentDataSQL/pomo/pomodoro"
 )
@@ -82,4 +84,24 @@ func (r *inMemoryRepo) Breaks(n int) ([]pomodoro.Interval, error) {
 		}
 	}
 	return data, nil
+}
+
+func (r *inMemoryRepo) CategorySummary(day time.Time, filter string) (time.Duration, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	var d time.Duration
+
+	filter = strings.Trim(filter, "%")
+
+	for _, i := range r.intervals {
+		if i.StartTime.Year() == day.Year() &&
+			i.StartTime.YearDay() == day.YearDay() {
+			if strings.Contains(i.Category, filter) {
+				d += i.ActualDuration
+			}
+		}
+	}
+
+	return d, nil
 }
---
+++ persistentDataSQL/pomo/pomodoro/summary.go
@@ -0,0 +1,26 @@
+package pomodoro
+
+import "time"
+
+func DailySummary(day time.Time, config *IntervalConfig) ([]time.Duration, error) {
+	dPomo, err := config.repo.CategorySummary(day, CategoryPomodoro)
+	if err != nil {
+		return nil, err
+	}
+
+	dBreaks, err := config.repo.CategorySummary(day, "%Break")
+	if err != nil {
+		return nil, err
+	}
+
+	return []time.Duration{
+		dPomo,
+		dBreaks,
+	}, nil
+}
+
+type LineSeries struct {
+	Name   string
+	Labels map[int]string
+	Values []float64
+}
