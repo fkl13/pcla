358316029dea186bbda42c7767f9571dec4a23d9
Fabian Klaiber <fklaiber@posteo.net>
2025-10-15 22:11:02 +0200

    Add integration test

---
+++ distributing/notify/integration_test.go
@@ -0,0 +1,19 @@
+//go:build integration
+// +build integration
+
+package notify_test
+
+import (
+	"testing"
+
+	"github.com/fkl13/pcla/distributing/notify"
+)
+
+func TestSend(t *testing.T) {
+	n := notify.New("test title", "test msg", notify.SeverityNormal)
+
+	err := n.Send()
+	if err != nil {
+		t.Error(err)
+	}
+}
